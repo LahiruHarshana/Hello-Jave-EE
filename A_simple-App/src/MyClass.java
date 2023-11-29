c3ccb160906ba47af6ff07dceea1c1a8c574cfab
LahiruHarshana <lharshana2002@gmail.com>
2023-11-29 23:33:06 +0530

    updated

--- A_simple-App/src/MyClass.java
+++ A_simple-App/src/MyClass.java
@@ -1,5 +1,15 @@
-public class MyClass {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns={"/customer"})
+
+public class MyClass extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("request recieved");
     }
 }
