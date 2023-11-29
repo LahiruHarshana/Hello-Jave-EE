e7fdaaee07303519b78d2110444fb54d85ba3be0
LahiruHarshana <lharshana2002@gmail.com>
2023-11-29 23:48:20 +0530

    updated

--- A_simple-App/src/MyClass.java
+++ A_simple-App/src/MyClass.java
@@ -13,6 +13,6 @@ public class MyClass extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        Writer.write("")
+        Writer.write("Response Genarated");
     }
 }
