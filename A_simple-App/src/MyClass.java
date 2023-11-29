e2847d98361ce6e01ff6dcee43c8d00e24101db3
LahiruHarshana <lharshana2002@gmail.com>
2023-11-30 00:06:49 +0530

    updated

--- A_simple-App/src/MyClass.java
+++ A_simple-App/src/MyClass.java
@@ -8,7 +8,6 @@ import java.io.PrintWriter;
 import java.io.Writer;
 
 @WebServlet(urlPatterns="/customer")
-
 public class MyClass extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
