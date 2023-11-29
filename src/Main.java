c105d0ac6ea23b27c5817a8c405ac3cdaa9f5e40
LahiruHarshana <lharshana2002@gmail.com>
2023-11-29 22:34:38 +0530

    updated

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,5 @@ public class Main extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
     }
 }
